3daf2fdc11451e304d06e37701845a7cccf6fb0d
Shiluco <hirap1939@gmail.com>
2024-07-09 19:03:52 +0900

    fix:independent initializeTasks

--- src/component/pages/Home.tsx
+++ src/component/pages/Home.tsx
@@ -4,42 +4,42 @@ import { useEffect, useState } from 'react';
 import { Task } from '../../types/Task';
 import MakeTaskButton from '../templates/MakeTaskButton';
 
-const Home = () => {
-  const initializeTasks = (): Task[] => {
-    const storedOptions = localStorage.getItem('localTaskList');
+const initializeTasks = (): Task[] => {
+  const storedOptions = localStorage.getItem('localTaskList');
 
-    if (storedOptions) {
-      const parsedOptions = JSON.parse(storedOptions);
-      return parsedOptions.map((task: Task) => ({
-        ...task,
-        timeLimit: new Date(task.timeLimit),
-      }));
-    } else {
-      return [
-        {
-          id: 1,
-          title: 'Task 1',
-          isDone: false,
-          timeLimit: new Date('2024-06-23T23:59'),
-          taskDetail: 'Detail of Task 1',
-        },
-        {
-          id: 2,
-          title: 'Task 2',
-          isDone: true,
-          timeLimit: new Date('2024-07-01T23:59'),
-          taskDetail: 'Detail of Task 2',
-        },
-        {
-          id: 3,
-          title: 'Task 3',
-          isDone: false,
-          timeLimit: new Date('2024-07-15T23:59'),
-          taskDetail: 'Detail of Task 3',
-        },
-      ];
-    }
-  };
+  if (storedOptions) {
+    const parsedOptions = JSON.parse(storedOptions);
+    return parsedOptions.map((task: Task) => ({
+      ...task,
+      timeLimit: new Date(task.timeLimit),
+    }));
+  } else {
+    return [
+      {
+        id: 1,
+        title: 'Task 1',
+        isDone: false,
+        timeLimit: new Date('2024-06-23T23:59'),
+        taskDetail: 'Detail of Task 1',
+      },
+      {
+        id: 2,
+        title: 'Task 2',
+        isDone: true,
+        timeLimit: new Date('2024-07-01T23:59'),
+        taskDetail: 'Detail of Task 2',
+      },
+      {
+        id: 3,
+        title: 'Task 3',
+        isDone: false,
+        timeLimit: new Date('2024-07-15T23:59'),
+        taskDetail: 'Detail of Task 3',
+      },
+    ];
+  }
+};
+const Home = () => {
 
   const [taskList, setTaskList] = useState<Task[]>(initializeTasks);
 
